2e201b9077e911e8872e33b67dea6b9eb56e2f1b
“anpr” <anish.pr@oracle.com>
2018-06-26 09:50:11 +0530

    fully functional

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
-
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var cheerio = require('cheerio');
+var myLatestReviews = [];
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
  * The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well as
@@ -20,8 +24,8 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
         },
         card: {
             type: 'Simple',
-            title: `SessionSpeechlet - ${title}`,
-            content: `SessionSpeechlet - ${output}`,
+            title: `NowRunning ${title}`,
+            content: `NowRunning - ${output}`,
         },
         reprompt: {
             outputSpeech: {
@@ -35,11 +39,8 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
 
 
 
-var http = require('http');
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
-var cheerio = require('cheerio');
-var myLatestReviews = [];
+var copyRightSpeech = 'According to the now running website : '
+var count = 0;
 function webScrap(element) {
   return new Promise(function (resolve, reject) {
     var url = element['link'][0];
@@ -51,43 +52,124 @@ function webScrap(element) {
 
       response.on('end', function () {
         var $ = cheerio.load(json);
+        var shortReview = $("#ctl00_ContentPlaceHolderMainContent_ReviewSummary").text();
+        var rating = $("#ctl00_ContentPlaceHolderMainContent_RatingText").text();
         console.log(element.title[0]);
-        console.log($("#ctl00_ContentPlaceHolderMainContent_ReviewSummary").text());
-        console.log($("#ctl00_ContentPlaceHolderMainContent_RatingText").text());
+        myLatestReviews.push(element.title[0] +': '+ shortReview + ' Rating is : ' + rating)
+        console.log(shortReview);
+        console.log(rating);
         resolve('success');
       });
     });
     request.on('error', function (err) {
       console.log(err);
+       myLatestReviews.push('I did not understand that!')
       reject('ellame pochu');
     });
   });
 }
 parser.on('error', function (err) { console.log('Parser error', err); });
-async function getReview(callback) {
-  var data = '';
+async function getTopMovies(callback) {
+    count = 0
+    myLatestReviews = [];
+  return new Promise(function(resolve,reject){var data = '';
   http.get('http://www.nowrunning.com/cgi-bin/rss/reviews_malayalam.xml', async function (res) {
     if (res.statusCode >= 200 && res.statusCode < 400) {
       res.on('data', function (data_) { data += data_.toString(); });
       res.on('end', async function () {
         parser.parseString(data, async function (err, result) {
-          for (let i = 0; i < 4; i++) {
-            callb(result['rss']['channel'][0]['item'][i])
+          for (let i = 0; i < 3; i++) {
+            callb(result['rss']['channel'][0]['item'][i], callback)
           }
         });
       });
     }
   });
-   const cardTitle = 'hello';
-    const speechOutput = 'kooi';
-    // Setting this to true ends the session and exits the skill.
-    const shouldEndSession = false;
-    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, false));
+  });
 };
 
-async function callb(element) {
+async function getMovieSuggestions(callback) {
+    count = 0
+    let randomNumber = Math.floor(Math.random() * 10) + 1  
+    myLatestReviews = []
+  return new Promise(function(resolve,reject){var data = '';
+  http.get('http://www.nowrunning.com/cgi-bin/rss/reviews_malayalam.xml', async function (res) {
+    if (res.statusCode >= 200 && res.statusCode < 400) {
+      res.on('data', function (data_) { data += data_.toString(); });
+      res.on('end', async function () {
+        parser.parseString(data, async function (err, result) {
+            getMovieFromIndex(result['rss']['channel'][0]['item'][randomNumber], callback)
+        });
+      });
+    }
+  });
+  });
+};
+
+async function getMovieFromIndex(element, callback) {
   console.log(element.title[0]);
   await webScrap(element)
+   count++;
+  if(count ==1){
+    console.log(myLatestReviews.join())
+    const cardTitle = 'Suggest a movie';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+    callback({}, buildSpeechletResponse(cardTitle, copyRightSpeech + myLatestReviews.join(), null, shouldEndSession));
+  }
+}
+
+async function callb(element, callback) {
+  console.log(element.title[0]);
+  await webScrap(element)
+   count++;
+  if(count ==3){
+    console.log(myLatestReviews.join())
+    const cardTitle = 'Top movies currently';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+    callback({}, buildSpeechletResponse(cardTitle,copyRightSpeech + myLatestReviews.join(), null, shouldEndSession));
+  }
+}
+
+function test(){
+getMovieSuggestions().then(()=> {
+console.log('hello');
+}).catch(()=> console.log('error'));
+}
+
+test();
+
+
+function getBestMoviesOfTheYear(callback, intent){
+count = 0
+let year = intent.slots.year.value
+console.log(intent.slots.year.value);
+console.log('reached slot value')
+if(!intent.slots.year.value){
+    year = 2018;
+}
+let myLatestReviews = []
+let reviewLink = 'http://www.nowrunning.com/top-rated-malayalam-movies/' + year +'/';
+var request = http.get(reviewLink, function (response) {
+ var json = '';
+ response.on('data', function (chunk) {
+   json += chunk;
+ });
+
+ response.on('end', function () {
+   var $ = cheerio.load(json);
+   for( let k=0 ; k < 5; k++){
+    myLatestReviews.push($('.BestMoviesRight').eq(k).find('a').eq(0).text() + ' Has a ' + $('.BestMoviesRight').eq(k).find('span').eq(0).text() +
+    $('.BestMoviesRight').eq(k).find('span').eq(1).text());
+   }
+   console.log(myLatestReviews);
+ callback({}, buildSpeechletResponse('top movies of the year' , copyRightSpeech + myLatestReviews.join(' '), null, true));
+ });
+});
+request.on('error', function (err) {
+ console.log(err); 
+});
 }
 
 
@@ -106,12 +188,26 @@ function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     const sessionAttributes = {};
     const cardTitle = 'Welcome';
-    const speechOutput = 'Welcome to the Alexa Skills Kit sample. ' +
-        'Please tell me your favorite color by saying, my favorite color is red';
+    const speechOutput = 'Welcome to the Alexa Skill For Movie Reviews. ' +
+        'Please ask me about top movies of the week or suggest a random movie or top movies of the year';
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
-    const repromptText = 'Please tell me your favorite color by saying, ' +
-        'my favorite color is red';
+    const repromptText = 'Please ask me about malayalam movies, ' +
+        'Example, Top movies of the week, best movies, review of movie name, suggest a random movie, ask now running for current movies, ask now running to suggest a movie to watch, ask now running for top movies of the two thousand eighteen';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function errorResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'I did not understand that'
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Please ask me about malayalam movies, ' +
+        'Example, Top movies of the week, best movies, review of movie name, suggest a random movie, ask now running for current movies, ask now running to suggest a movie to watch, ask now running for top movies of the two thousand eighteen';
     const shouldEndSession = false;
 
     callback(sessionAttributes,
@@ -120,7 +216,7 @@ function getWelcomeResponse(callback) {
 
 function handleSessionEndRequest(callback) {
     const cardTitle = 'Session Ended';
-    const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
+    const speechOutput = 'Thank you for trying the Alexa Skills for Malayalam movie reviews. Have a nice day!';
     // Setting this to true ends the session and exits the skill.
     const shouldEndSession = true;
 
@@ -136,55 +232,7 @@ function createFavoriteColorAttributes(favoriteColor) {
 /**
  * Sets the color in the session and prepares the speech to reply to the user.
  */
-function setColorInSession(intent, session, callback) {
-    const cardTitle = intent.name;
-    const favoriteColorSlot = intent.slots.Color;
-    let repromptText = '';
-    let sessionAttributes = {};
-    const shouldEndSession = false;
-    let speechOutput = '';
 
-    if (favoriteColorSlot) {
-        const favoriteColor = favoriteColorSlot.value;
-        sessionAttributes = createFavoriteColorAttributes(favoriteColor);
-        speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
-            "your favorite color by saying, what's my favorite color?";
-        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
-    } else {
-        speechOutput = "I'm not sure what your favorite color is. Please try again.";
-        repromptText = "I'm not sure what your favorite color is. You can tell me your " +
-            'favorite color by saying, my favorite color is red';
-    }
-
-    callback(sessionAttributes,
-         buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-
-function getColorFromSession(intent, session, callback) {
-    let favoriteColor;
-    const repromptText = null;
-    const sessionAttributes = {};
-    let shouldEndSession = false;
-    let speechOutput = '';
-
-    if (session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-    }
-
-    if (favoriteColor) {
-        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
-        shouldEndSession = true;
-    } else {
-        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-            ' is red';
-    }
-
-    // Setting repromptText to null signifies that we do not want to reprompt the user.
-    // If the user does not respond or says something that is not understood, the session
-    // will end.
-    callback(sessionAttributes,
-         buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-}
 
 
 // --------------- Events -----------------------
@@ -215,19 +263,18 @@ function onIntent(intentRequest, session, callback) {
     const intent = intentRequest.intent;
     const intentName = intentRequest.intent.name;
 
-    // Dispatch to your skill's intent handlers
-    if (intentName === 'MyColorIsIntent') {
-        setColorInSession(intent, session, callback);
-    } else if (intentName === 'WhatsMyColorIntent') {
-        getColorFromSession(intent, session, callback);
-    } else if (intentName === 'AMAZON.HelpIntent') {
+ if (intentName === 'AMAZON.HelpIntent') {
         getWelcomeResponse(callback);
        } else if (intentName === 'GetTopMovies') {
-        getReview(callback);
-    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        getTopMovies(callback);
+       } else if (intentName === 'GetMovieSuggestion') {
+        getMovieSuggestions(callback);
+       } else if (intentName === 'GetBestMovieOfYear') {
+         getBestMoviesOfTheYear(callback, intent);
+       } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
         handleSessionEndRequest(callback);
     } else {
-        throw new Error('Invalid intent');
+        errorResponse(callback);
     }
 }
 
